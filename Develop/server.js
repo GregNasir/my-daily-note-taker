3473c0cd0e5013e6c1e88cfff87ad7c720c51e68
GregNasir <63434657+GregNasir@users.noreply.github.com>
2023-06-30 00:36:08 -0400

    fixed but in title link in index.js

--- Develop/server.js
+++ Develop/server.js
@@ -1,61 +1,81 @@
-const express = require('express');
-const path = require('path');
-const fs = require('fs');
-const util = require('util');
-const uniqid = require('uniqid');
+const fs = require('fs')
+const path = require('path')
+const express = require('express')
+
 const PORT = process.env.PORT || 3001;
-const noteData = require('./db/db.json')
-// const { allowedNodeEnvironmentFlags } = require('process');
+const db = require('./db/db.json')
 
-const app = express();
+//Allows all notes to have a unique ID
+const { v4: uuidv4 } = require('uuid');
 
-//const getAndRenderNotes = require('./public/assets/js/index')
-
-app.use(express.json());
+//Allows public folder to be unblocked
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'))
+app.use(express.json())
 
-app.use(express.static('public'));
+const app = express()
 
-
-//app.get('/', (req, res) => res.send('Navigate to the public folder'))
-
-app.get('/notes', (req, res) => 
-    res.sendFile(path.join(__dirname, 'public/notes.html'))
-);
-
+//API Routes
+// GET /api/notes should read the db.json file and return all saved notes as JSON.
 app.get('/api/notes', (req, res) => {
-    //res.readFile(path.join(__dirname, 'db/db.json'))
-    //res.json(noteData);
-    //console.log(noteData);
-    res.json(noteData);
-});
+    fs.readFile('./db/db.json', (err, data) => {
+        ///error logging
+        if (err) throw err;
+        let dbData = JSON.parse(data);
+        //Returns new database
+        res.json(dbData)
+    });   
+})
 
-
-
+//POST 
+///api/notes receives a new note to save on the request body and add it to db.json, then returns new note to the client.
 app.post('/api/notes', (req, res) => {
-    console.log('in the post route');
-            console.log('in the else of ReadandAppend');
-            var newNote = req.body;
-            console.log('this is before the id', newNote);
-            newNote.id = uniqid();
-            console.log('this is after the id is added', newNote);
-            noteData.push(newNote);
-            fs.writeFile('./db/db.json', JSON.stringify(noteData, null, 4) , (err) => {
-            err ? console.log(err) : res.send(newNote)
-        })
-});
+    //grabs notes from body of request
+    const newNote = req.body
 
-app.delete('api/notes/:id', (req, res) => {
-    const id = req.params.id;
-    res.unlink(id, './db/db.json')
-    res.readFile(id, './db/db.json')
-});
+    //gives each note a random ID
+    newNote.id = uuidv4()
+
+    //adds the note object to the array
+    db.push(newNote)
+
+    //update the json file with the new object
+    fs.writeFileSync('./db/db.json', JSON.stringify(db))
+
+    //responds with the note object used
+    res.json(db)
+})
 
 
-app.get('*', (req, res) =>
-    res.sendFile(path.join(__dirname, 'public/index.html'))
-);
+//DELETE
+// notes when the button is clicked by removing the note from db.json, saving and showing the updated database on the front end.
+app.delete('/api/notes/:id', (req, res) => {
+    const newDb = db.filter((note) =>
+        note.id !== req.params.id)
 
-app.listen(PORT, () => {
-    console.log(`Example app listening at http://localHost:${PORT}`);
-});
+    // update the db.json file to reflect the modified notes array
+    fs.writeFileSync('./db/db.json', JSON.stringify(newDb))
+
+    // send that removed note object back to user
+    readFile.json(newDb)
+})
+
+//HTML Routes
+//Home
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
+
+//Notes
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'notes.html'))
+})
+
+//Wildcard Route
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
+
+//App listens with front end on this port
+app.listen(PORT, () =>
+    console.log(`App listening on ${PORT}`))
